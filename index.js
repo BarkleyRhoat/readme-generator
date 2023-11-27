8e0d7e1cabc6c1c473e1e7a511a1066655a58c51
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-11-26 23:14:34 -0500

    deleted some things in the questions

--- index.js
+++ index.js
@@ -10,68 +10,67 @@ const questions = [
       type: 'input',
       name: 'title',
       message: 'What is the project name?',
-      validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
         type: 'input',
         message: 'Describe the purpose of this project?',
         name: 'description',
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+        
       },
     {
       type: 'input',
       message: 'Instructions to follow?',
       name: 'instructions',
-      validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      
     },
     {
         type: 'input',
         message: 'Please provide url that will deploy this app.',
         name: 'link',
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+        
       },
       {
         type: 'input',
         message: 'provide relative path to the image of the screenshots.',
         name: 'screenshot',
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+        
       },
     {
         type: 'input',
         message: 'are there any contributors (use Github usernames)?',
         name: 'contributors',
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+        
       },
       {
-        type: 'list',
+        type: 'checkbox',
         message: 'which license did you use for this project?',
         name: 'license',
         choice: ['the MIT License', 'the GPL License', 'Apache license', 'GNU license', 'N/A'],
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+       
       },
       {
         type: 'input',
         message: 'provide walkthrough of the app?',
         name: 'test',
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+        
       },
       {
         type: 'input',
         message: 'what are the technologies and languages used in this project?',
         name: 'usage',
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+       
       },
       {
         type: 'input',
         message: 'Github username?',
         name: 'creator',
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      
       },
       {
         type: 'input',
         message: 'e-mail?',
         name: 'email',
-        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+        
       }
       
   ]
