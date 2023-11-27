e2532547bf52fe8da18c9bb1d0164f715f7b26b2
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-11-26 22:53:56 -0500

    completed all of the project

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-inquirer
-  .prompt([
+// Created an array of questions for user input
+const questions = [
     {
       type: 'input',
       name: 'title',
@@ -13,11 +13,11 @@ inquirer
       validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
     },
     {
-      type: 'input',
-      message: 'how do you install the app?',
-      name: 'installation',
-      validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
-    },
+        type: 'input',
+        message: 'Describe the purpose of this project?',
+        name: 'description',
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      },
     {
       type: 'input',
       message: 'Instructions to follow?',
@@ -26,30 +26,66 @@ inquirer
     },
     {
         type: 'input',
-        message: 'are there any credits?',
-        name: 'credits',
+        message: 'Please provide url that will deploy this app.',
+        name: 'link',
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      },
+      {
+        type: 'input',
+        message: 'provide relative path to the image of the screenshots.',
+        name: 'screenshot',
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      },
+    {
+        type: 'input',
+        message: 'are there any contributors (use Github usernames)?',
+        name: 'contributors',
         validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
       },
       {
         type: 'list',
-        message: 'What is your preferred method of communication?',
-        name: 'contact',
-        choices: ['email', 'phone', 'telekinesis'],
+        message: 'which license did you use for this project?',
+        name: 'license',
+        choice: ['the MIT License', 'the GPL License', 'Apache license', 'GNU license', 'N/A'],
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
       },
-  ])
-// const questions = [];
+      {
+        type: 'input',
+        message: 'provide walkthrough of the app?',
+        name: 'test',
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      },
+      {
+        type: 'input',
+        message: 'what are the technologies and languages used in this project?',
+        name: 'usage',
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      },
+      {
+        type: 'input',
+        message: 'Github username?',
+        name: 'creator',
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      },
+      {
+        type: 'input',
+        message: 'e-mail?',
+        name: 'email',
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      }
+      
+  ]
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
-    // ... ask the questions here
-    let responses = {
-        title: ""
-    }
-    console.log(generateMarkdown(responses));
+// function to write README file
+function writeToFile(fileName, data) {
+return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// Function call to initialize app
-init();
+// function to initialize app
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log ("creating Professional README.md File..");
+        writeToFile("./misc", generateMarkdown({ ...responses}));
+    });
+}
+    init();
