75c81cfcd21d79748a5bc8987f953039a1e30a82
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-11-26 21:08:51 -0500

    added promts

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the project name?',
+      validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+    },
+    {
+      type: 'input',
+      message: 'how do you install the app?',
+      name: 'installation',
+      validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+    },
+    {
+      type: 'input',
+      message: 'Instructions to follow?',
+      name: 'instructions',
+      validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+    },
+    {
+        type: 'input',
+        message: 'are there any credits?',
+        name: 'credits',
+        validate: (value)=>{ if(value){return true} else {return 'i need a value to continue'}},
+      },
+      {
+        type: 'list',
+        message: 'What is your preferred method of communication?',
+        name: 'contact',
+        choices: ['email', 'phone', 'telekinesis'],
+      },
+  ])
+// const questions = [];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
